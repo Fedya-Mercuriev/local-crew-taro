dc576ee08a72d22284a4967cc5797a33c9669c52
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2018-01-01 10:40:14 +0700

    Добавлен корневой vue-файл

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="app-wrapper">
+        <TextInput></TextInput>
+    </div>
+</template>
+
+<script>
+    import TextInput from './components/text-input';
+    export default {
+        name: 'App',
+        components: {
+            TextInput
+        }
+    }
+</script>
+
+<style lang="scss">
+    html, body, .app-wrapper {
+        position: relative;
+        margin: 0;
+        height: 100%;
+    }
+</style>
