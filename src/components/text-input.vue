b6efbc14bcc875ca7f79c4c4e4fad67c82416391
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2018-01-01 10:40:41 +0700

    Заверстана форма, реализовано базовое поведение

---
+++ src/components/text-input.vue
@@ -0,0 +1,163 @@
+<template>
+    <form js-fortunetelling-form class="form form_center" action="" method="get">
+        <div class="form-group">
+            <label js-text-input class="field" :class="{ 'shrink-left': confirmationRequested }">
+                <span class="field__label" :class="{ 'is-visible': label.length }">{{ label }}</span>
+                <span class="field__main"></span>
+                <span class="field__indicator"></span>
+                <input class="field-input field__input"
+                       v-model="question" type="text"
+                       name="question" value=""
+                       placeholder="На что будем гадать?">
+            </label>
+            <button type="submit"
+                    class="button button_stick_right button_modal button_red"
+                    :class="{ 'button_hidden': !confirmationRequested }">Давай</button>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "TextInput",
+        data() {
+            return {
+                question: '',
+                label: '',
+                confirmationRequested: false,
+                timerId: null
+            }
+        },
+        watch: {
+            question(newQuestion, oldQuestion) {
+                if (newQuestion.length > 0 && newQuestion.length < 4) {
+                    clearTimeout(this.timerId);
+                    this.confirmationRequested = false;
+                    this.label = 'Коротковат вопрос...';
+                } else if (newQuestion.length === 0) {
+                    clearTimeout(this.timerId);
+                    this.confirmationRequested = false;
+                    this.label = '';
+                } else if (this.getStringLocale() !== 'ru') {
+                    clearTimeout(this.timerId);
+                    this.label = 'По-русски!';
+                } else if (newQuestion.length > 25) {
+                    this.label = 'Воу-воу! Полегче!';
+                } else if (newQuestion.length > 50) {
+                    this.label = 'Серьезно, остановись';
+                } else {
+                    clearTimeout(this.timerId);
+                    this.timerId = setTimeout(() => {
+                        this.requestInputConfirm();
+                    },400)
+                }
+            }
+        },
+        methods: {
+            //TODO: Поправить регулярное выражение
+            getStringLocale() {
+                if (/[\u0400-\u04FF]+$/.test(this.question)) {
+                    return 'ru';
+                }
+                return 'other';
+            },
+            triggerLayOutCards() {
+                this.$emit('layout-cards', Math.random());
+            },
+            requestInputConfirm() {
+                this.confirmationRequested = true;
+                this.label = 'Похоже на вопрос. Делаю расклад?'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .form {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%);
+        min-width: 280px;
+        max-width: 1240px;
+    }
+    .form-group {
+        position: relative;
+        display: flex;
+    }
+    .field {
+        position: relative;
+        z-index: 10;
+        box-sizing: border-box;
+        width: 100%;
+        transition: all 0.3s;
+        &:hover {
+            &__main {
+                box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+            }
+        }
+        &__main {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            border-radius: 8px;
+            box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+            transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+        }
+        &__placeholder {
+            display: block;
+            @media screen and (max-width: 320px) {
+                text-align: center;
+            }
+        }
+        &__label {
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform: translateY(0);
+            font-size: 12px;
+            font-family: 'Roboto', sans-serif;
+            opacity: 0;
+            transition: all 0.3s;
+        }
+        &__label.is-visible {
+            transform: translateY(-20px);
+            opacity: 1;
+        }
+    }
+    .field.shrink-left {
+        width: 73%;
+    }
+    .field-input {
+        box-sizing: border-box;
+        display: block;
+        width: 100%;
+        padding: 15px;
+        background-color: transparent;
+        border-color: transparent;
+        -webkit-appearance: none;
+        outline: none;
+    }
+    .button {
+        box-sizing: border-box;
+        padding: 16px;
+        border: 0;
+        border-radius: 8px;
+        font-size: 13px;
+        font-family: 'Roboto', sans-serif;
+        -webkit-appearance: none;
+        cursor: pointer;
+        transition: all 0.35s;
+    }
+    .button_hidden {
+        opacity: 0;
+    }
+    .button_stick_right {
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: -1;
+    }
+</style>
