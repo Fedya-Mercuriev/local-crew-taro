bc6c71a6adbc126c40062b1ba2e06fce3c2ec733
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-11-18 00:00:43 +0700

    Начат компонент с столом для карт

---
+++ src/components/cards-layout.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "CardsLayout",
+        props: [
+            'cards'
+        ],
+        data() {
+            return {}
+        },
+        methods: {
+            init() {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
