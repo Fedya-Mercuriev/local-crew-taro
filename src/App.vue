40eabd20da23dc3cfef18df9c53b598007ccbf08
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-11-18 00:01:45 +0700

    Добавлены заглушки для карт, подборка случайных карт для раскалада

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="app">
         <div class="app__content">
-            <TextInput v-on:layout-cards="showSome($event)"></TextInput>
-            <CardsLayout></CardsLayout>
+            <TextInput v-on:layout-cards="setCardsNum($event)"></TextInput>
+            <CardsLayout :cards="selectedCards"></CardsLayout>
         </div>
     </div>
 </template>
@@ -18,18 +18,72 @@
       data() {
           return {
               cards: [
+                  {
+                      id: 1,
+                      image: '',
+                      title: 'XXX',
+                      description: '....'
+                  },
+                  {
+                      id: 2,
+                      image: '',
+                      title: 'YYY',
+                      description: '....'
+                  },
+                  {
+                      id: 3,
+                      image: '',
+                      title: 'ZZZ',
+                      description: '....'
+                  },
+                  {
+                      id: 4,
+                      image: '',
+                      title: 'AAA',
+                      description: '....'
+                  },
 
               ],
-              curdsNum: 0
+              cardsNum: 0,
+              selectedCards: []
+          }
+      },
+      watch: {
+          cardsNum(newValue) {
+              this.getCardsPack(newValue);
           }
       },
       methods: {
-        showSome(maxCards) {
-          alert(maxCards)
+          setCardsNum(cardsNumber) {
+              this.cardsNum = cardsNumber;
+          },
+        getCardsPack(cardsNumber) {
+              for (let i = 0; i < cardsNumber; i++) {
+                  this.selectedCards.push(this.getRandomCard());
+              }
         },
-        getRandomCards() {
+          getRandomCard() {
+              const index = Math.floor(Math.random() * (this.cards.length - 1 + 1) + 1) - 1;
+              let selectedCard = this.cards[index];
 
-        }
+              if (this.cardIsUnique(selectedCard)) {
+                  return selectedCard;
+              } else {
+                  selectedCard = this.getRandomCard();
+              }
+              return selectedCard;
+          },
+          cardIsUnique(card) {
+              let isUnique = true;
+              if (!this.selectedCards.length) return isUnique;
+              for (let i = 0; i < this.selectedCards.length; i++) {
+                  if (card.id === this.selectedCards[i].id) {
+                      isUnique = false;
+                      break;
+                  }
+              }
+              return isUnique;
+          }
       }
     }
 </script>
@@ -41,6 +95,7 @@
         height: 100%;
     }
     .app {
+        box-sizing: border-box;
         height: 100%;
         padding: 20px 0;
         &__content {
